08471d38a5ad38b9006e1c4005b7243b59d70969
TrippleSeven777 <133012114+killerboyyy777@users.noreply.github.com>
2025-10-24 22:17:31 +0200

    Please work

--- index.js
+++ index.js
@@ -463,7 +463,8 @@ if (cluster.isWorker) {
       community.getSteamUser(SENDER, (errUser, user) => {
         if (errUser) {
           logError(
-          `Failure parsing users Steam Info. Possibly illegal ASCII letters in name OR steam failed to fetch info: ${errUser}`
+          'Failure parsing users Steam Info. Possibly illegal ASCII letters in name OR ' +
+          `steam failed to fetch info: ${errUser}`
           );
           return; 
         }
@@ -596,8 +597,17 @@ if (cluster.isWorker) {
           MSG.toUpperCase() === '!RATES' ||
           MSG.toUpperCase() === '!PRICES'
         ) {
-          const priceMsg1 = `Sell Your: \r\n1 TF2 Key for Our ${CONFIG.Rates.SELL.TF2_To_Gems} Gems\r\n\r\nBuy Our: \r\n1 TF2 Key for Your ${CONFIG.Rates.BUY.Gems_To_TF2_Rate} Gems\r\n\r\nWe're also:\r\n`;
-          const priceMsg2 = `Buying Your Backgrounds & Emotes for ${CONFIG.Rates.BUY.BG_And_Emotes} Gems (Send offer & add correct number of my gems for auto accept.)\r\nSelling any of OUR Backgrounds & Emotes for ${CONFIG.Rates.SELL.BG_And_Emotes} Gems (Send offer & add correct number of my gems for auto accept.)`;
+          const priceMsg1 = 'Sell Your: \r\n'
+            + `1 TF2 Key for Our ${CONFIG.Rates.SELL.TF2_To_Gems} Gems\r\n\r\n`
+            + 'Buy Our: \r\n'
+            + `1 TF2 Key for Your ${CONFIG.Rates.BUY.Gems_To_TF2_Rate} Gems\r\n\r\n`
+            + "We're also:\r\n";
+          const priceMsg2 = 'Buying Your Backgrounds & Emotes for '
+            + `${CONFIG.Rates.BUY.BG_And_Emotes} Gems `
+            + '(Send offer & add correct number of my gems for auto accept.)\r\n'
+            + 'Selling any of OUR Backgrounds & Emotes for '
+            + `${CONFIG.Rates.SELL.BG_And_Emotes} Gems `
+            + '(Send offer & add correct number of my gems for auto accept.)';
           client.chatMessage(
             SENDER,
             priceMsg1 + priceMsg2
